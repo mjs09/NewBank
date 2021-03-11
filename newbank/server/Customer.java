6d17859fb996941cdc938ee8f1d9c8cbabaa860f
Jorge Costa da Silva <jorgecsilva@gmail.com>
2021-03-11 13:00:23 +0000

    SHOWMYACCOUNTS command

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -3,22 +3,22 @@ package newbank.server;
 import java.util.ArrayList;
 
 public class Customer {
-	
+
 	private ArrayList<Account> accounts;
-	
+
 	public Customer() {
 		accounts = new ArrayList<>();
 	}
-	
+
 	public String accountsToString() {
 		String s = "";
-		for(Account a : accounts) {
-			s += a.toString();
+		for (Account a : accounts) {
+			s += a.toString() + ", ";
 		}
 		return s;
 	}
 
 	public void addAccount(Account account) {
-		accounts.add(account);		
+		accounts.add(account);
 	}
 }
--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -5,35 +5,41 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class NewBank {
-	
+
 	private static final NewBank bank = new NewBank();
-	private HashMap<String,Customer> customers;
-	
+	private HashMap<String, Customer> customers;
+
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
-	
+
 	private void addTestData() {
 		Customer bhagy = new Customer();
 		bhagy.addAccount(new Account("Main", 1000.0));
+		bhagy.addAccount(new Account("Savings", 10000.0));
+		bhagy.addAccount(new Account("Checking", 150.0));
 		customers.put("Bhagy", bhagy);
-		
+
 		Customer christina = new Customer();
+		christina.addAccount(new Account("Main", 150.0));
 		christina.addAccount(new Account("Savings", 1500.0));
+		christina.addAccount(new Account("Checking", 250.0));
 		customers.put("Christina", christina);
-		
+
 		Customer john = new Customer();
+		john.addAccount(new Account("Main", 300.0));
+		john.addAccount(new Account("Savings", 3000.0));
 		john.addAccount(new Account("Checking", 250.0));
 		customers.put("John", john);
 	}
-	
+
 	public static NewBank getBank() {
 		return bank;
 	}
-	
+
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
-		if(customers.containsKey(userName)) {
+		if (customers.containsKey(userName)) {
 			return new CustomerID(userName);
 		}
 		return null;
@@ -41,10 +47,12 @@ public class NewBank {
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request) {
-		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-			case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-			default : return "FAIL";
+		if (customers.containsKey(customer.getKey())) {
+			switch (request) {
+			case "SHOWMYACCOUNTS":
+				return showMyAccounts(customer);
+			default:
+				return "FAIL";
 			}
 		}
 		return "FAIL";
@@ -52,22 +60,24 @@ public class NewBank {
 
 	// commands from the NewBank customer are processed in this method
 	public synchronized String processRequest(CustomerID customer, String request, String accountName, double balance) {
-		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-				case "NEWACCOUNT" : return addAccount(customer, accountName, balance);
-				default : return "FAIL";
+		if (customers.containsKey(customer.getKey())) {
+			switch (request) {
+			case "NEWACCOUNT":
+				return addAccount(customer, accountName, balance);
+			default:
+				return "FAIL";
 			}
 		}
 		return "FAIL";
 	}
-	
+
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
 	}
 
 	private String addAccount(CustomerID customer, String accountName, double balance) {
-		customers.get(customer.getKey()).addAccount(new Account(accountName,balance));
-	    return "SUCCESS";
+		customers.get(customer.getKey()).addAccount(new Account(accountName, balance));
+		return "SUCCESS";
 	}
 
 }
