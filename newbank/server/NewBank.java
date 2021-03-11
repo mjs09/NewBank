492d40ee654d6b5c917be0ab5498989c542348e8
abidemiraji <80225232+abidemiraji@users.noreply.github.com>
2021-03-12 00:52:43 +0100

    Update NewBank.java

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -3,7 +3,7 @@ package newbank.server;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import jave.util.Scanner;
+import java.util.Scanner;
 
 public class NewBank {
 
